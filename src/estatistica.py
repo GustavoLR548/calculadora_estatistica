e46f0672cbedd441d908e91a430615c052c5c0ff
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-03-12 16:06:22 -0300

    feat: agora é possível calcular variancia, primeiro e terceiro quartil

--- src/estatistica.py
+++ src/estatistica.py
@@ -24,7 +24,7 @@ def mediana(list):
 def moda(list): 
     return ([item for item, count in collections.Counter(list).items() if count > 1])
 
-def desvio_padrao(list,ma):
+def variancia(list,ma):
     resultado = 0
 
     for x in list: 
@@ -32,11 +32,35 @@ def desvio_padrao(list,ma):
 
     resultado /= len(list) -1
 
-    return sqrt(resultado)
+    return resultado
     
 def coeficiente_de_variacao(list,dp,ma):
     return dp/ma*100
 
+def primeiro_quartil(list):
+    list.sort()
+    result = 0.0
+    size = len(list) 
+    if(size%2 == 0):
+        result = media_aritmetica(list[1:int(size/2-1)])
+    else: 
+        result = media_aritmetica(list[1:int(size/2)])
+    
+    return result
+
+def terceiro_quartil(list):
+    list.sort()
+    result = 0.0
+    size = len(list) 
+    if(size%2 == 0):
+        result = media_aritmetica(list[int(size/2+2):int(size-1)])
+    else: 
+        result = media_aritmetica(list[int(size/2+1):int(size-1)])
+    
+    return result
+
+
+ 
 
 input_string = input('Insira os números:\n(Obs: Coloque todos os números separados por espaço)\n->')
 
@@ -47,8 +71,12 @@ print("\nResultado:\n")
 ma = media_aritmetica(number_list)
 me = mediana(number_list)
 mo = moda(number_list)
-dp = desvio_padrao(number_list,ma)
+va = variancia(number_list,ma)
+dp = sqrt(va)
 cv = coeficiente_de_variacao(number_list,dp,ma)
+
+q1 = primeiro_quartil(number_list)
+q3 = terceiro_quartil(number_list)
 
 tipo_de_moda = ""
 
@@ -62,9 +90,13 @@ else:
     tipo_de_moda = "multimodal"
 
 print("Media aritmética:        " + str(ma))
-print("Mediana:                 " + str(me))
+print("Mediana/2°Quartil:       " + str(me))
 print("Moda:                    " + str(mo) + " " + tipo_de_moda)
-print("Desvio Padrão:           " + str(dp))
-print("Coeficiente de variação: " + str(cv))
+print("Variância:               " + str(va) + "%")
+print("Desvio Padrão:           " + str(dp) + "%")
+print("Coeficiente de variação: " + str(cv) + "%") 
+
+print("Primeiro quartil:        " + str(q1)) 
+print("Terceiro quartil:        " + str(q3)) 
 
 
